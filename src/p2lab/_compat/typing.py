096e0f0f05bae4a3430fa4c5ed96fe15d2b3dbaf
Nathan Simpson <nsimpson@turing.ac.uk>
2023-06-10 19:47:24 +0100

    some changes

--- src/p2lab/_compat/typing.py
+++ src/p2lab/_compat/typing.py
@@ -7,12 +7,7 @@ P2-Lab: a secret package for genetic optimisation of pokemon teams
 
 from __future__ import annotations
 
-import sys
-
-if sys.version_info < (3, 8):
-    from typing_extensions import Literal, Protocol, runtime_checkable
-else:
-    from typing import Literal, Protocol, runtime_checkable
+from typing import Literal, Protocol, runtime_checkable
 
 __all__ = ["Protocol", "runtime_checkable", "Literal"]
 
