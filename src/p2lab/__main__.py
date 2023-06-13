b8961bb947e1a3e78a1aeef14807706db4afb74c
AoifeHughes <ahughes@turing.ac.uk>
2023-06-13 10:49:39 +0100

    changes and things to making teams

--- src/p2lab/__main__.py
+++ src/p2lab/__main__.py
@@ -5,14 +5,17 @@ TODO: Write some docs here.
 from .teams.builder import Builder
 from .evaluator.poke_env import PokeEnv
 
-def main():
+N_generations = 100 # Number of generations to run
+N_teams = 100 # Number of teams to generate per generation
 
-    # Main expected loop
-    N_generations = 100 # Number of generations to run
+def main():
+    builder = Builder()
     curr_gen = 0 # Current generation
-    poke_pool = [] # List of Pokemon 
+    poke_pool = builder.make_N_teams(N_teams) # List of Pokemon 
+
     teams = [] # list of teams (of Pokemon)
 
+    # Main expected loop
     while curr_gen < N_generations:
         team_fitness = [] # list of scores (of teams)
         poke_pool = [] # List of Pokemon 
--- src/p2lab/teams/builder.py
+++ src/p2lab/teams/builder.py
@@ -2,13 +2,23 @@
 The team builder
 """
 
-class Builder:
+from subprocess import check_output
+from poke_env.teambuilder import Teambuilder
+import numpy as np
+import sys
+
+class Builder(Teambuilder):
     """
     The team builder
     """
+    def __init__(self, teams=None, format="gen2randombattle"):
+        if teams:
+            self.teams = [self.join_team(self.parse_showdown_team(team)) for team in teams]
+        else:
+            self.teams = [self.join_team(self.parse_showdown_team(check_output(f"pokemon-showdown generate-team {format}| pokemon-showdown export-team", shell=True).decode(sys.stdout.encoding)) )]
 
-    def __init__(self):
-        pass 
+    def yield_team(self):
+        return np.random.choice(self.teams)
 
     def make_team(self):
         pass
