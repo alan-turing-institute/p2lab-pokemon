7b06cde2e5359f6fbf33bee3fe81cd738cccce94
AoifeHughes <ahughes@turing.ac.uk>
2023-06-13 12:20:02 +0100

    moving towards battle readiness

--- src/p2lab/__main__.py
+++ src/p2lab/__main__.py
@@ -11,13 +11,13 @@ N_teams = 3 # Number of teams to generate per generation
 def main():
     builder = Builder(N_seed_teams=N_teams)
     builder.build_N_teams_from_poke_pool(N_teams)
-
     curr_gen = 0 # Current generation
     teams = [builder.yield_team() for n in range(N_teams)]
-
+    evaluator = PokeEnv()
     # Main expected loop
     while curr_gen < N_generations:
-        team_fitness = [] # list of scores (of teams)
+        team_fitness = evaluator.evaluate_teams(teams)
+        
         poke_pool = [] # List of Pokemon 
         teams = [] # list of teams (of Pokemon)
         curr_gen += 1
--- src/p2lab/evaluator/poke_env.py
+++ src/p2lab/evaluator/poke_env.py
@@ -4,3 +4,6 @@
 class PokeEnv:
     def __init__(self):
         pass
+
+    def evaluate_teams(self, teams):
+        pass
