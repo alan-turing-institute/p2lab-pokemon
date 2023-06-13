37d7e11076bd37b397658ae1e86c46aac7f1583e
AoifeHughes <ahughes@turing.ac.uk>
2023-06-13 11:20:27 +0100

    fixed pokepool generation

--- src/p2lab/teams/builder.py
+++ src/p2lab/teams/builder.py
@@ -11,17 +11,21 @@ class Builder(Teambuilder):
     """
     The team builder
     """
-    def __init__(self, teams=None, format="gen2randombattle"):
+    def __init__(self, N_seed_teams=2, teams=None, format="gen2randombattle"):
         if teams:
             self.teams = [self.join_team(self.parse_showdown_team(team)) for team in teams]
         else:
-            self.teams = [self.join_team(self.parse_showdown_team(check_output(f"pokemon-showdown generate-team {format}| pokemon-showdown export-team", shell=True).decode(sys.stdout.encoding)) )]
+            self.teams = []
+            for _ in range(N_seed_teams):
+                self.teams.append(self.parse_showdown_team(check_output(f"pokemon-showdown generate-team {format}| pokemon-showdown export-team", shell=True).decode(sys.stdout.encoding)))
+        self.poke_pool = np.array(self.teams).flatten()
+        
 
     def yield_team(self):
         return np.random.choice(self.teams)
 
     def make_team(self):
         pass
-
+    
     def make_N_teams(self, N):
         return [self.make_team() for _ in range(N)]
