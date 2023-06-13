fe32bcd03d362efa11567d1990e95c06ef2fd7d1
Boyko Vodenicharski <bvodenicharski@turing.ac.uk>
2023-06-13 14:07:50 +0100

    Rm test lines

--- src/p2lab/genetic/matching.py
+++ src/p2lab/genetic/matching.py
@@ -1,44 +1,39 @@
+from __future__ import annotations
+
 import networkx as nx
 import numpy as np
 
-def dense(teams : list[object]) -> np.ndarray:
-	r"""
-	Generates a fully connected set of edges. Each team is 
-	connected to every other one.
 
-	Inputs:
-		:teams: set of teams.
+def dense(teams: list[object]) -> np.ndarray:
+    r"""
+    Generates a fully connected set of edges. Each team is
+    connected to every other one.
 
-	Outputs:
-	
-		np.ndarray shape (N_edges, 2)
-		
-	"""
-	g = nx.complete_graph(len(teams))
+    Inputs:
+            :teams: set of teams.
 
-	# Select only the node index pairs, exclude the feature dict.
-	return np.stack( g.edges(data=False) )
+    Outputs:
 
-def mst(teams : list[object]) -> np.ndarray:
-	r"""
-	Returns a minimum spanning tree graph generated from the
-	fully connected graph over the teams.
+            np.ndarray shape (N_edges, 2)
 
-	Outputs:
+    """
+    g = nx.complete_graph(len(teams))
 
-		np.ndarray shape (N_edges, 2)
+    # Select only the node index pairs, exclude the feature dict.
+    return np.stack(g.edges(data=False))
 
-	"""
-	g = nx.complete_graph(len(teams))
-	mst = nx.algorithms.tree.minimum_spanning_edges(g, data=False)
 
-	return list(mst)
+def mst(teams: list[object]) -> np.ndarray:
+    r"""
+    Returns a minimum spanning tree graph generated from the
+    fully connected graph over the teams.
 
-def main():
-	
-	# Test
-	print(mst([0] * 5))
-	print(dense([0] * 5))
+    Outputs:
 
-if __name__ == "__main__":
-	main()
+            np.ndarray shape (N_edges, 2)
+
+    """
+    g = nx.complete_graph(len(teams))
+    mst = nx.algorithms.tree.minimum_spanning_edges(g, data=False)
+
+    return list(mst)
