9ff992b34d1c6b64235365b6733c74d3b7b37031
AoifeHughes <ahughes@turing.ac.uk>
2023-06-13 11:40:37 +0100

    teams are now generated from a pool

--- src/p2lab/__main__.py
+++ src/p2lab/__main__.py
@@ -4,16 +4,16 @@ TODO: Write some docs here.
 
 from .teams.builder import Builder
 from .evaluator.poke_env import PokeEnv
-
+from tqdm import tqdm
 N_generations = 100 # Number of generations to run
 N_teams = 100 # Number of teams to generate per generation
 
 def main():
-    builder = Builder()
-    curr_gen = 0 # Current generation
-    poke_pool = builder.make_N_teams(N_teams) # List of Pokemon 
+    builder = Builder(N_seed_teams=N_teams)
+    builder.build_N_teams_from_poke_pool(N_teams)
 
-    teams = [] # list of teams (of Pokemon)
+    curr_gen = 0 # Current generation
+    teams = [builder.yield_team() for n in range(N_teams)]
 
     # Main expected loop
     while curr_gen < N_generations:
