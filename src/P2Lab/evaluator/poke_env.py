8ca7c92d135291e5baf7dbde77b387f15ba25b23
AoifeHughes <ahughes@turing.ac.uk>
2023-06-14 08:19:09 +0100

    I had it set to doubles because I'm a moron

--- src/P2Lab/evaluator/poke_env.py
+++ src/P2Lab/evaluator/poke_env.py
@@ -5,11 +5,12 @@ import numpy as np
 from itertools import combinations
 from poke_env.player import Player, RandomPlayer
 from poke_env import PlayerConfiguration
-from tqdm import tqdm
+
+
 class PokeEnv:
-    def __init__(self, n_battles=100):
-        self.p1 = RandomPlayer(PlayerConfiguration("Player 1", None), battle_format="gen8doublescustomgame")
-        self.p2 = RandomPlayer(PlayerConfiguration("Player 2", None),battle_format="gen8doublescustomgame")
+    def __init__(self, n_battles=100, battle_format="gen7anythinggoes"):
+        self.p1 = RandomPlayer(PlayerConfiguration("Player 1", None), battle_format=battle_format)
+        self.p2 = RandomPlayer(PlayerConfiguration("Player 2", None), battle_format=battle_format)
         self.n_battles = n_battles
         pass
 
@@ -23,7 +24,7 @@ class PokeEnv:
 
 
     async def battle(self, team1, team2):
-        self.p1.update_team("]".join([mon.formatted for mon in team1.get_teamlist()]))
-        self.p2.update_team("]".join([mon.formatted for mon in team2.get_teamlist()]))
+        self.p1.update_team(team1.to_packed_str())
+        self.p2.update_team(team2.to_packed_str())
         await self.p1.battle_against(self.p2, n_battles=self.n_battles)
         return self.p1.n_won_battles, self.p2.n_won_battles
--- src/P2Lab/teams/team.py
+++ src/P2Lab/teams/team.py
@@ -5,16 +5,15 @@ class Team:
     def __init__(self, pokemon) -> None:
         self.pokemon = pokemon
         self.fitness = 0
-    
+
     def get_teamlist(self):
         return self.pokemon
+    
     def get_fitness(self):
         return self.fitness
-    def random_mutate(self, p: float, pokemon_population: list[str]):
-        print(p)
-        print(pokemon_population)
-        # randomly replace one team member with probability p
-        self.pokemon = self.pokemon
-
+    
     def set_fitness(self, x):
         self.fitness = x
+
+    def to_packed_str(self):
+        return "]".join([mon.formatted for mon in self.pokemon])
