322391f4386ae874727bd226cd812abe27d86863
Nathan Simpson <nsimpson@turing.ac.uk>
2023-06-14 13:48:51 +0100

    change rename

--- src/P2Lab/_compat/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Copyright (c) 2023 Scientists of the P2 Laboratory. All rights reserved.
-
-P2-Lab: a secret package for genetic optimisation of pokemon teams
-"""
-
-
-from __future__ import annotations
