7dfc74767c2e02c7139b44d56b19baaf2c528c60
Nathan Simpson <nsimpson@turing.ac.uk>
2023-06-15 10:17:14 +0100

    IT WORKS

--- run.py
+++ run.py
@@ -1,12 +1,16 @@
 from __future__ import annotations
 
+import asyncio
 import sys
 from subprocess import check_output
 
 import numpy as np
+from poke_env import PlayerConfiguration
+from poke_env.player import RandomPlayer
 from poke_env.teambuilder import Teambuilder
 from tqdm import tqdm
 
+from p2lab.genetic.matching import dense
 from p2lab.team import Team
 
 
@@ -56,8 +60,48 @@ def generate_teams(pool, num_teams, team_size=6):
     ]
 
 
+async def run_battles(
+    matches,
+    teams,
+    battles_per_match=1,
+    bot_strategy="random",
+    battle_format="gen7anythinggoes",
+    progress_bar=True,
+):
+    if bot_strategy == "random":
+        p1 = RandomPlayer(
+            PlayerConfiguration("Player 1", None), battle_format=battle_format
+        )
+        p2 = RandomPlayer(
+            PlayerConfiguration("Player 2", None), battle_format=battle_format
+        )
+    else:
+        msg = f"bot strategy not implemented: should be one of ['random'] but got {bot_strategy}"
+        raise Exception(msg)
+
+    results = []
+    for t1, t2 in tqdm(matches) if progress_bar else matches:
+        p1.update_team(teams[t1].to_packed_str())
+        p2.update_team(teams[t2].to_packed_str())
+        await p1.battle_against(p2, n_battles=battles_per_match)
+        results.append((p1.n_won_battles, p2.n_won_battles))
+        p1.reset_battles()
+        p2.reset_battles()
+
+    return results
+
+
+async def main():
+    pool = generate_pool(10)
+    print(len(pool))
+    teams = generate_teams(pool, 3)
+    matches = dense(teams)
+    print(f"matches: {matches}")
+    print(f"matches shape: {matches.shape}")
+    results = await run_battles(matches, teams)
+    print(f"results: {results}")
+
+
 # test the pool generation
 if __name__ == "__main__":
-    pool = generate_pool(100)
-    print(pool)
-    print(len(pool))
+    asyncio.get_event_loop().run_until_complete(main())
