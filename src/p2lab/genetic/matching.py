79b0a5cd75a85278903ba50cb8e1820b17d88373
Boyko Vodenicharski <bvodenicharski@turing.ac.uk>
2023-06-15 11:26:44 +0100

    Remove nx dependency

--- src/p2lab/genetic/matching.py
+++ src/p2lab/genetic/matching.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
-import networkx as nx
+from itertools import combinations
+
+# import networkx as nx
 import numpy as np
 
 
@@ -17,23 +19,20 @@ def dense(teams: list[object]) -> np.ndarray:
             np.ndarray shape (N_edges, 2)
 
     """
-    g = nx.complete_graph(len(teams))
-
-    # Select only the node index pairs, exclude the feature dict.
-    return np.stack(g.edges(data=False))
+    return list(combinations(range(len(teams)), 2))
 
 
-def mst(teams: list[object]) -> np.ndarray:
-    r"""
-    Returns a minimum spanning tree graph generated from the
-    fully connected graph over the teams.
-
-    Outputs:
-
-            np.ndarray shape (N_edges, 2)
-
-    """
-    g = nx.complete_graph(len(teams))
-    mst = nx.algorithms.tree.minimum_spanning_edges(g, data=False)
-
-    return list(mst)
+# def mst(teams: list[object]) -> np.ndarray:
+#    r"""
+#    Returns a minimum spanning tree graph generated from the
+#    fully connected graph over the teams.
+#
+#    Outputs:
+#
+#            np.ndarray shape (N_edges, 2)
+#
+#    """
+#    g = nx.complete_graph(len(teams))
+#    mst = nx.algorithms.tree.minimum_spanning_edges(g, data=False)
+#
+#    return list(mst)
