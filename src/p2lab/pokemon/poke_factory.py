b4b16416604f592453f5c5cad247ad5e67b76c24
AoifeHughes <ahughes@turing.ac.uk>
2023-06-20 11:43:43 +0100

    added init gen framework

---
+++ src/p2lab/pokemon/poke_factory.py
@@ -0,0 +1,21 @@
+"""
+This class is used to generate Pokemon teams using the pokedex provided from
+Pokemon Showdown (via poke-env)
+
+This is directly inspired by poke-env's diagostic_tools folder
+"""
+from __future__ import annotations
+
+from poke_env.data import GenData as POKEDEX
+
+
+class PokeFactory:
+    def __init__(self, gen=1):
+        self.dex = POKEDEX(gen)
+        self.dex2mon = {
+            self.dex.pokedex[m]["num"]: self.dex.pokedex[m]["num"]
+            for m in self.dex.pokedex
+        }
+
+    def get_pokemon_by_dexnum(self, dexnum):
+        return self.dex.pokedex[self.dex2mon[dexnum]]
---
+++ tests/test_pokedex.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from p2lab.pokemon import poke_factory
+
+
+def test_pokedex():
+    p = poke_factory.PokeFactory()
+    assert p is not None
+
+
+def test_eevee_fetch():
+    p = poke_factory.PokeFactory()
+    eevee = p.get_pokemon_by_dexnum(133)
+    assert eevee["species"] == "Eevee"
