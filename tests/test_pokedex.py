4265adbb029fdf2d039acd9623a871139fb55603
AoifeHughes <ahughes@turing.ac.uk>
2023-06-20 12:59:31 +0100

    added tests to drive dev

--- tests/test_pokedex.py
+++ tests/test_pokedex.py
@@ -12,3 +12,15 @@ def test_eevee_fetch():
     p = poke_factory.PokeFactory()
     eevee = p.get_pokemon_by_dexnum(133)
     assert eevee["species"].lower() == "eevee"
+
+
+def test_eevee_moves():
+    assert 1 == 0
+
+
+def test_eevee_stats():
+    assert 1 == 0
+
+
+def test_impossible_pokemon():
+    assert 1 == 0
