bae34ca406d607a7f4c52951dec66915d545bf86
AoifeHughes <ahughes@turing.ac.uk>
2023-06-20 16:08:17 +0100

    Added more features and tests

--- src/p2lab/pokemon/poke_factory.py
+++ src/p2lab/pokemon/poke_factory.py
@@ -10,9 +10,35 @@ from poke_env.data import GenData as POKEDEX
 
 
 class PokeFactory:
-    def __init__(self, gen=1):
-        self.dex = POKEDEX(gen)
-        self.dex2mon = {int(self.dex.pokedex[m]["num"]): m for m in self.dex.pokedex}
+    def __init__(self, gen=1, drop_forms=True):
+        self.gen = (
+            gen if gen > 4 else 3
+        )  # because this seems to be the minimum gen for the pokedex
+        self.dex = POKEDEX(self.gen)
+        if drop_forms:
+            self.dex2mon = {
+                int(self.dex.pokedex[m]["num"]): m
+                for m in self.dex.pokedex
+                if "forme" not in self.dex.pokedex[m].keys()
+            }
+        else:
+            self.dex2mon = {
+                int(self.dex.pokedex[m]["num"]): m for m in self.dex.pokedex
+            }
 
     def get_pokemon_by_dexnum(self, dexnum):
         return self.dex.pokedex[self.dex2mon[dexnum]]
+
+    def get_allowed_moves(self, dexnum, level=100):
+        pot_moves = self.dex.learnset[self.dex2mon[dexnum]]["learnset"]
+        allowed_moves = []
+        for move, lims in pot_moves.items():
+            gens = [int(lim[0]) for lim in lims]
+            # TODO: write logic here to check if level is allowed
+            if level != 100:
+                msg = "Level checking not implemented yet"
+                raise NotImplementedError(msg)
+            # lvls = [int(l[1:]) for l in lims if l[1:].isdigit()]
+            if self.gen in gens:
+                allowed_moves.append(move)
+        return allowed_moves
--- tests/test_pokedex.py
+++ tests/test_pokedex.py
@@ -11,18 +11,26 @@ def test_pokedex():
 def test_eevee_fetch():
     p = poke_factory.PokeFactory()
     eevee = p.get_pokemon_by_dexnum(133)
-    assert eevee["species"].lower() == "eevee"
+    assert eevee["baseSpecies"].lower() == "eevee"
+
+
+def test_bulbasaur_fetch():
+    p = poke_factory.PokeFactory()
+    eevee = p.get_pokemon_by_dexnum(1)
+    assert eevee["baseSpecies"].lower() == "bulbasaur"
 
 
 def test_eevee_moves():
     p = poke_factory.PokeFactory()
-    eevee = p.get_pokemon_by_dexnum(133)
-    assert eevee is not None
+    eevee_moves = p.get_allowed_moves(133)
+    assert len(eevee_moves) > 0
+
+
+def test_bulbasaur_moves():
+    p = poke_factory.PokeFactory()
+    bulb_moves = p.get_allowed_moves(1)
+    assert len(bulb_moves) > 0
 
 
 def test_eevee_stats():
-    assert 1 == 0
-
-
-def test_impossible_pokemon():
     assert 1 == 0
