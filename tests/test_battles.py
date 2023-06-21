3326f2ccee1b71786af51b5250879078b21ad3c9
AoifeHughes <ahughes@turing.ac.uk>
2023-06-21 09:50:12 +0100

    Added initial battle testing test

--- tests/test_battles.py
+++ tests/test_battles.py
@@ -1,15 +1,31 @@
 from __future__ import annotations
 
+import pytest
+from poke_env import PlayerConfiguration
+from poke_env.player import SimpleHeuristicsPlayer
 
-def test_battle_random_pokes():
-    assert True
+from p2lab.battling.battles import run_battles
+from p2lab.pokemon import pokefactory
+from p2lab.pokemon.teams import Team
+
+pytest_plugins = ("pytest_asyncio",)
 
 
-#     p = pokefactory.PokeFactory()
-#     eevee = p.make_pokemon(133, moves=["tackle", "growl"], level=5)
-#     pikachu = p.make_pokemon(25, moves=["thundershock", "growl"], level=5)
-# team1 = Team([eevee])
-# team2 = Team([pikachu])
-# #matches =
+@pytest.mark.asyncio()
+async def test_battle_random_pokes():
+    p = pokefactory.PokeFactory()
+    eevee = p.make_pokemon(133, moves=["tackle", "growl"], level=5)
+    pikachu = p.make_pokemon(25, moves=["thundershock", "growl"], level=5)
+    team1 = Team([eevee])
+    team2 = Team([pikachu])
+    teams = [team1, team2]
+    matches = [[0, 1]]
 
-# res = battle.run_battles(matches, teams, player_1, player_2, battles_per_match=10)
+    player_1 = SimpleHeuristicsPlayer(
+        PlayerConfiguration("Player 1", None), battle_format="gen7anythinggoes"
+    )
+    player_2 = SimpleHeuristicsPlayer(
+        PlayerConfiguration("Player 2", None), battle_format="gen7anythinggoes"
+    )
+    res = await run_battles(matches, teams, player_1, player_2, battles_per_match=1)
+    assert res is not None
